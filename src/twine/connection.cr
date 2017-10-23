365bd8ed965d262d155ac174b0316c9b17eff8fc
Cihangir SAVAS <cihangir@savas.io>
2017-10-23 15:01:39 +0300

    docker: add REDIS_URL env var

--- src/twine/connection.cr
+++ src/twine/connection.cr
@@ -5,11 +5,8 @@ module Twine
     property redis : Redis
 
     def initialize
-      url = ENV["REDIS_URL"].to_s
-      puts "redis url before #{@url}!"
       ENV["REDIS_URL"] ||= "redis://localhost:6379"
       url = ENV["REDIS_URL"].to_s
-      puts "redis url before #{@url}!"
       
       @redis = Redis.new url: url
       at_exit do
